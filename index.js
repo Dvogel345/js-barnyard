693dade53e36015f9ee1547dbc23fc3f8e3bc0dc
jeremyrrose <jeremy.r.rose@gmail.com>
2021-05-31 01:03:37 -0400

    reload function added to context

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const welcome = 
 `
-\n
 Welcome to the JavaScript Barnyard! 
+Please play with the animals. Enjoy the fresh air!
 See README.md or visit https://github.com/jeremyrrose/js-barnyard for directions.
-\n
 `
 console.log(welcome)
 
@@ -12,12 +11,20 @@ const replServer = repl.start({
     prompt: "JS Barnyard > "
 })
 
-const classes = require('./classes.js')
-for (const [k,v] of Object.entries(classes)) {
-    replServer.context[k] = v
+const loadContext = () => {
+
+    Object.keys(require.cache).forEach(function(key) { delete require.cache[key] })
+    
+    const classes = require('./classes.js')
+    for (const [k,v] of Object.entries(classes)) {
+        replServer.context[k] = v
+    }
+
+    const instances = require('./instances.js')
+    for (const [k,v] of Object.entries(instances)) {
+        replServer.context[k] = v
+    }
 }
 
-const instances = require('./instances.js')
-for (const [k,v] of Object.entries(instances)) {
-    replServer.context[k] = v
-}
+loadContext()
+replServer.context.reload = loadContext
